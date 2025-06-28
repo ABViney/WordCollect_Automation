158d245534f25cd7fbb905067c87c920866575b9
ABViney <vineyadrian@gmail.com>
2025-06-28 18:37:04 -0400

    Define scope

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+/**
+* Creating a program that automates playing WordCollect (Wordscapes clone). I joined a program where I can play mobile
+* games and get rewards based on my progress/actions in the game.
+*
+* The max reward for this game is $200, tho the money trickles in over the course of thousands of levels.
+*
+* This program is designed to automate playing this game for me.
+*/
