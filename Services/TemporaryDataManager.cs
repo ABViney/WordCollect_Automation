101e73f315ffeb4092800d156dd36bd854a9c53a
ABViney <vineyadrian@gmail.com>
2025-06-29 22:47:46 -0400

    Add service for managing temporary files

---
+++ Services/TemporaryDataManager.cs
@@ -0,0 +1,70 @@
+namespace WordCollect_Automated.Services;
+
+public interface ITemporaryDirectory : IDisposable
+{
+    string Path { get; }
+    void EnsureExists();
+}
+
+public interface ITemporaryFile : IDisposable
+{
+    string Path { get; }
+}
+
+public static class TemporaryDataManager
+{
+    static TemporaryDataManager()
+    {
+        if (Directory.Exists(Path.ToTemporaryData))
+        {
+            Directory.Delete(Path.ToTemporaryData, true);
+        }
+        Directory.CreateDirectory(Path.ToTemporaryData);
+    }
+
+    public static ITemporaryDirectory CreateTemporaryDirectory() =>
+        new TemporaryDirectory(System.IO.Path.Combine(Path.ToTemporaryData, System.IO.Path.GetRandomFileName()));
+    
+    public static ITemporaryFile CreateTemporaryFile() =>
+            new TemporaryFile(System.IO.Path.Combine(Path.ToTemporaryData, System.IO.Path.GetRandomFileName()));
+
+    public static void CleanUpTemporaryFiles()
+    {
+        if (Directory.Exists(Path.ToTemporaryData))
+        {
+            Directory.Delete(Path.ToTemporaryData);
+        }
+    }
+
+    private class TemporaryDirectory : ITemporaryDirectory
+    {
+        public string Path { get; }
+        
+        public void EnsureExists() => Directory.CreateDirectory(Path);
+
+        public TemporaryDirectory(string path)
+        {
+            Path = path;
+        }
+        
+        public void Dispose()
+        {
+            Directory.Delete(Path, true);
+        }
+    }
+
+    private class TemporaryFile : ITemporaryFile
+    {
+        public string Path { get; }
+
+        public TemporaryFile(string path)
+        {
+            Path = path;
+        }
+        
+        public void Dispose()
+        {
+            File.Delete(Path);
+        }
+    }
+}
