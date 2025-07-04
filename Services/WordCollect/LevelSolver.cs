3f479b74ef39c26cc865bc695ac7cd6891871e30
abviney <vineyadrian@gmail.com>
2025-07-04 01:53:32 -0400

    Refactor for readability

--- Services/WordCollect/LevelSolver.cs
+++ Services/WordCollect/LevelSolver.cs
@@ -9,18 +9,35 @@ namespace WordCollect_Automated.Services.WordCollect;
 /// </summary>
 public class LevelSolver
 {
-    private string _window;
+    // Window details
+    private string _windowName;
     private BoundingBox _windowBoundingBox;
+    
+    // Service for scanning screenshots for selectable letters and their locations
     private SelectableLetterParser _selectableLetterParser;
+    
+    // Set up a controller to take over the mouse and program inputs
     private MouseController _mouseController;
+    
+    // Fuzzies the point an input occurs at to make it look less robotic
+    private BiasedRadialPointFuzzer _pointFuzzer;
+    
+    // Interpolates movement between points to smooth out touch movements
+    private IStrokeInterpolator _pointLerp;
 
-    public LevelSolver(string window)
+    public LevelSolver(string windowName)
     {
-        _window = window;
-        _windowBoundingBox = GnomeDesktop.GetWindowBoundingBox(_window);
+        
+        _windowName = windowName;
+        _windowBoundingBox = GnomeDesktop.GetWindowBoundingBox(_windowName);
+        
         _selectableLetterParser = new SelectableLetterParser();
+        
         var desktopBoundingBox = GnomeDesktop.GetDesktopBoundingBox();
         _mouseController = new MouseController(desktopBoundingBox.Width, desktopBoundingBox.Height);
+        
+        _pointFuzzer = new BiasedRadialPointFuzzer(30);
+        _pointLerp = new LinearStrokeInterpolator();
     }
 
     /// <summary>
@@ -29,8 +46,8 @@ public class LevelSolver
     public void Solve()
     {
         // Snap a picture of the current level
-        ITemporaryFile screenshot = TemporaryDataManager.CreateTemporaryFile();
-        GnomeDesktop.ScreenshotWindow(_window, screenshot.Path);
+        ITemporaryFile screenshot = TemporaryDataManager.CreateTemporaryPNGFile();
+        GnomeDesktop.ScreenshotWindow(_windowName, screenshot.Path);
         
         // Todo: Test to make sure that a level is presented and no popups are blocking the screen before continuing
         
@@ -45,12 +62,6 @@ public class LevelSolver
         // The wagon wheel is built with selectable letters so I can feed it the result from building a word to get the routed path
         WagonWheelPathingGraph<SelectableLetter> inputPaths = CreateWagonWheelPathingGraph(center, selectableLetterPool.SelectableLetters);
 
-        // Fuzzies the point an input occurs at to make it look less robotic
-        BiasedRadialPointFuzzer brpf = new BiasedRadialPointFuzzer(30);
-
-        // Interpolates movement between points to smooth out touch movements
-        IStrokeInterpolator lerp = new LinearStrokeInterpolator();
-        
         // Todo: Add persistent word blacklist
         // This loop is active while the user is trying to solve the puzzle
         bool puzzleIsSolved = false;
@@ -78,47 +89,62 @@ public class LevelSolver
             // No, the puzzle isn't solved
             // input next word
             string nextWordToTry = potentialWords.Pop();
-            Console.WriteLine($"Submitting word: {nextWordToTry}");
             
-            // Pick which letters in the pool will be used to enter this word.
-            IEnumerable<SelectableLetter> selectableLetterOrder = selectableLetterPool.BuildWord(nextWordToTry);
-            
-            // Get the points the cursor must pass through
-            IEnumerable<Point> path = inputPaths.ResolvePath(selectableLetterOrder).Select(sl =>
-            {
-                var selectableLetterBoundingBox = sl.BoundingBox;
-                var normalizedBoundingBox = _windowBoundingBox.Normalize(selectableLetterBoundingBox);
-                return brpf.Fuzz(normalizedBoundingBox.Center);
-            });
-            
-            // Create a route for the cursor to move
-            Queue<Point> interpolatedPath = new (lerp.Interpolate(path, 50));
-
-            // Focus the window
-            GnomeDesktop.FocusWindow(_window);
-            
-            // Move to the first letter
-            _mouseController.MoveTo(interpolatedPath.Dequeue());
-            _mouseController.Press(MouseButton.Left);
-            // Wait before beginning movements
-            Thread.Sleep(Random.Shared.Next(600, 1000));
-
-            foreach (var checkpoint in checkpoints)
-            {
-                // Give the UI time to update, this app is slow
-                int timeUntilMovement = Random.Shared.Next(300, 700);
-                Thread.Sleep(timeUntilMovement);
-                _mouseController.MoveTo(checkpoint);
-            }
-            // Wait before release
-            Thread.Sleep(Random.Shared.Next(600, 1000));
-            _mouseController.Release(MouseButton.Left);
+            InputWord(nextWordToTry, selectableLetterPool, inputPaths);
             
             // Wait a bit if the word is filling in the solved words pool
-            Thread.Sleep(Random.Shared.Next(1500, 4000));
+            Thread.Sleep(Random.Shared.Next(800, 1500));
         }
     }
 
+    /// <summary>
+    /// Input a word into the game.
+    /// </summary>
+    /// <param name="word">The word to spell out</param>
+    /// <param name="selectableLetterPool">The pool of selectable letters</param>
+    /// <param name="wagonWheelPathingGraph">A model of the characters on screen</param>
+    private void InputWord(string word, SelectableLetterPool selectableLetterPool, WagonWheelPathingGraph<SelectableLetter> wagonWheelPathingGraph)
+    {
+        Console.WriteLine($"Submitting word: {word}");
+        
+        // Pick which letters in the pool will be used to enter this word.
+        IEnumerable<SelectableLetter> selectableLetterOrder = selectableLetterPool.BuildWord(word);
+        
+        // Get the points the cursor must pass through
+        IEnumerable<Point> path = wagonWheelPathingGraph.ResolvePath(selectableLetterOrder).Select(sl =>
+        {
+            var selectableLetterBoundingBox = sl.BoundingBox;
+            var normalizedBoundingBox = _windowBoundingBox.Normalize(selectableLetterBoundingBox);
+            return _pointFuzzer.Fuzz(normalizedBoundingBox.Center);
+        });
+        
+        // Create a route for the cursor to move
+        Queue<Point> interpolatedPath = new (_pointLerp.Interpolate(path, 50));
+
+        // Focus the window
+        GnomeDesktop.FocusWindow(_windowName);
+            
+        Console.WriteLine($"Inputting: {word}");
+        
+        // Move to the first letter
+        _mouseController.MoveTo(interpolatedPath.Dequeue());
+        _mouseController.Press(MouseButton.Left);
+        // Wait before beginning movements
+        Thread.Sleep(Random.Shared.Next(200, 500));
+
+        foreach (var point in interpolatedPath)
+        {
+            // Give the UI time to update, this app is slow
+            int timeUntilMovement = Random.Shared.Next(5, 15);
+            Thread.Sleep(timeUntilMovement);
+            _mouseController.MoveTo(point);
+        }
+        
+        // Wait before release
+        Thread.Sleep(Random.Shared.Next(100, 300));
+        _mouseController.Release(MouseButton.Left);
+    }
+    
     private WagonWheelPathingGraph<SelectableLetter> CreateWagonWheelPathingGraph(SelectableLetter intermediarySelectable,
         List<SelectableLetter> selectableLetters)
     {
