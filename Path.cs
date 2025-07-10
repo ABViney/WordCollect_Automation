2cc47148f4b361056795babdec15b9798ef6ee4c
abviney <vineyadrian@gmail.com>
2025-07-10 14:29:48 -0400

    Adds files for template matching

--- Path.cs
+++ Path.cs
@@ -23,22 +23,43 @@ public static class Path
     // Files //
     ///////////
     
-    // In Assets/
+    // Databse
     public static string ToEnglishDictionaryDB => System.IO.Path.Combine(ToAssets, "EnglishDictionary.db");
+    
+    // Tesseract executable
     public static string ToTesseractExe => System.IO.Path.Combine(ToAssets, "tesseract*");
+    
+    // Masks
     public static string ToLetterPoolOverlay =>
         System.IO.Path.Combine(ToAssets, "be2028_letter-pool-overlay.png");
     public static string ToSolvedWordsOverlay =>
         System.IO.Path.Combine(ToAssets, "be2028_solved-words-overlay.png");
     public static string ToSolvedTileOverlay =>
         System.IO.Path.Combine(ToAssets, "be2028_general-tile-overlay.png");
+    
+    // Templates
     public static string ToLevelInProgressTemplate =>
-        System.IO.Path.Combine(ToAssets, "be2028_level-in-progress-template.png");
+        System.IO.Path.Combine(ToAssets, "level-in-progress-template_35x50+300+590.png");
+    public static string  ToLevelCompleteTemplate =>
+        System.IO.Path.Combine(ToAssets, "level-complete_20x20+220+320.png");
+    public static string  ToAppleTournamentPregameTemplate =>
+        System.IO.Path.Combine(ToAssets, "apple-tournament-pregame_20x20+85+515.png");
+    public static string  ToEndlessDealsTemplate =>
+        System.IO.Path.Combine(ToAssets, "endless-deals_40x20+310+175.png");
+    public static string  ToOutOfFirefliesTemplate =>
+        System.IO.Path.Combine(ToAssets, "out-of-fireflies_20x20+215+280.png");
+    public static string  ToPiggyBankFullTemplate =>
+        System.IO.Path.Combine(ToAssets, "piggy-bank-full_20x20+160+400.png");
+    public static string  ToSummerBloomsJigsawPuzzleTemplate =>
+        System.IO.Path.Combine(ToAssets, "summer-blooms-jigsaw-puzzle_20x20+350+230.png");
+    public static string  ToWildWordEventTemplate =>
+        System.IO.Path.Combine(ToAssets, "wild-word-event_20x20+345+330.png");
     
-    // In Temp/
-    public static string ToCurrentLetterPool => System.IO.Path.Combine(ToTemporaryData, "current-character-pool/");
+        
     
-    // Outputs of CharacterReader.cs
+        
+        
+        
     
     
 }
